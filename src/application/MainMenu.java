368ec78b3cc6ded8d1db53da698cbcb59298660a
thunder-tw <wardtruyen@gmail.com>
2015-10-12 20:38:30 +0200

    Verder afwerken Opdracht 1d
    Application package: Opdracht 1d
    Vakantiehuisjes beheren/registreren
    20% klaar

--- src/application/MainMenu.java
+++ src/application/MainMenu.java
@@ -3,6 +3,8 @@ package application;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -10,11 +12,32 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 //import javax.swing.
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class MainMenu {
-	public static void addComponentsToPane(Container pane) {
+@SuppressWarnings("serial")
+public class MainMenu extends JFrame{
+	
+	RegistrationManager manager;
+	
+	public MainMenu(String string) {
+		// super set Title constructor
+		super(string);
+		
+		// create the registrationManager
+		manager = new RegistrationManager();
+
+		// set up window/frame
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Set up the contents pane.
+		addComponentsToPane(this.getContentPane());
+
+		// Resize frame & components to fit.
+		this.pack();
+	}
+
+	private void addComponentsToPane(Container pane) {
 		
 		JPanel labelPanel = new JPanel();
 		addLabelsToPane(labelPanel);
@@ -27,7 +50,7 @@ public class MainMenu {
 		pane.add(buttonPanel, BorderLayout.PAGE_END);
 	}
 
-	public static void addLabelsToPane(Container pane) {
+	private void addLabelsToPane(Container pane) {
 		pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
 
 		JLabel emptyLabel = new JLabel("Huisjes: 107");
@@ -37,24 +60,36 @@ public class MainMenu {
 		pane.add(new JLabel("reservaties: 0"), BorderLayout.CENTER);
 	}
 
-	public static void addButtonsToPane(Container pane) {
+	private void addButtonsToPane(Container pane) {
 		pane.setLayout(new BoxLayout(pane, BoxLayout.X_AXIS));
 	
 		//pane.setLayout(new BorderLayout(4,0));
 
-		addAButton("Nieuwe reservatie", true, pane);
+		JButton btnNewResevation = addAButton("Nieuwe reservatie", true, pane);
+		btnNewResevation.addActionListener(new ActionListener() {
+	         public void actionPerformed(ActionEvent e) {
+	             // TODO: add what to do when the button is pressed
+	        	 JOptionPane.showMessageDialog(null, "TODO!");
+	        	 // openen venster met de nodige invoer velden
+	        	 
+	        	 // toevoegen registratie als het toevoegen niet geannuleerd werdt
+	        	 
+	        	 // indien geen exception werdt opgeworpen: registraties wegschrijven naar bestand
+	          }    
+		});
 		pane.add(Box.createRigidArea(new Dimension(4, 0)));
 		addAButton("Filter reservaties", false, pane);
 		pane.add(Box.createRigidArea(new Dimension(4, 0)));
 		addAButton("Reservatie annuleren", false, pane);
 	}
 
-	private static void addAButton(String text, boolean enabled, Container container) {
+	private JButton addAButton(String text, boolean enabled, Container container) {
 		JButton button = new JButton(text);
 		button.setEnabled(enabled);
 		//button.setAlignmentX(Component.CENTER_ALIGNMENT);
 		// button.setAlignmentY(Component.BOTTOM_ALIGNMENT);
 		container.add(button);
+		return button;
 	}
 
 	/**
@@ -63,17 +98,12 @@ public class MainMenu {
 	 */
 	private static void createAndShowGUI() {
 		// Create and set up the window.
-		JFrame frame = new JFrame("Vakantiehuisjes reserveren");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		// Set up the content pane.
-		addComponentsToPane(frame.getContentPane());
-
-		// Display the window.
-		 frame.pack();
-		 
-		//frame.setSize(400, 200);
+		JFrame frame = new MainMenu("Vakantiehuisjes reserveren");
+		
+		// Center window on screen
 		frame.setLocationRelativeTo(null);
+		
+		// Show
 		frame.setVisible(true);
 	}
 
---
+++ src/application/Registration.java
@@ -0,0 +1,28 @@
+package application;
+
+import model.Datum;
+
+public class Registration {
+	// wie
+	String voornaam;
+	String familienaam;
+	// waar / wat
+	int huisnummer; // rijnummer + volgnummer
+	// wanneer
+	Datum startDatum;
+	int aantalVerblijfNachten;
+	
+	public Registration( String voornaam, String familienaam, int huisnummer, Datum startDatum, int aantalVerblijfNachten )
+	{
+		// TODO: check valid values: Throw exception
+			// Datum > Now 
+			// String != null | empty 
+			// aantalVerblijfNachten > 0
+		
+		this.voornaam = voornaam;
+		this.familienaam = familienaam;
+		this.huisnummer = huisnummer;
+		this.startDatum = startDatum;
+		this.aantalVerblijfNachten = aantalVerblijfNachten;
+	}
+}
